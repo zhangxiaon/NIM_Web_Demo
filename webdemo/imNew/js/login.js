c4d38c4f7213b303fb76c1490d21f3504bb8fb71
zhangxiaonan <980948843@qq.com>
2021-01-13 16:38:56 +0800

    im demo

--- webdemo/imNew/js/login.js
+++ webdemo/imNew/js/login.js
@@ -2,7 +2,6 @@ var Login = {
   init: function() {
     this.initNode();
     this.showNotice();
-    this.initAnimation();
     this.addEvent();
   },
 
@@ -80,25 +79,10 @@ var Login = {
   },
   //这里做了个伪登录方式（实际上是把accid，token带到下个页面连SDK在做鉴权）
   //一般应用服务器的应用会有自己的登录接口
-  requestLogin: function(account, pwd) {
-    setCookie('uid', account.toLocaleLowerCase());
-    //自己的appkey就不用加密了
-    // setCookie('sdktoken',pwd);
-    if (
-      window.CONFIG.appkey === '' ||
-      window.CONFIG.appkey === ''
-    ) {
-      var password = MD5(pwd);
-    } else {
-      password = pwd;
-    }
-    setCookie('sdktoken', password);
-    if (/chatroom/.test(location.href)) {
-      delCookie('nickName');
-      window.location.href = './list.html';
-    } else {
-      window.location.href = './main.html';
-    }
+  requestLogin: function() {
+    setCookie('uid', window.CONFIG.uid);
+    setCookie('sdktoken', window.CONFIG.token);
+    window.location.href = './main.html';
   },
 
   /**
--- webdemo/imNew/js/main.js
+++ webdemo/imNew/js/main.js
@@ -6,17 +6,4 @@ var userUID = readCookie("uid")
  * 实例化
  * @see module/base/js
  */
-var yunXin
-// 等待私有化配置请求完毕
-if (CONFIG.usePrivateEnv === 1) {
- function waitPrivateConf() {
-   if (CONFIG.privateConf || CONFIG.usePrivateEnv === 2) {
-     yunXin = new YX(userUID)
-   } else {
-     setTimeout(waitPrivateConf, 1000)
-   }
- }
- waitPrivateConf()
-} else {
-  yunXin = new YX(userUID)
-}
+var yunXin = new YX(userUID)
